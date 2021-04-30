6f10f4149fc101d6849ff6ed960984743d012fb5
Andras Lasso <lasso@queensu.ca>
2021-04-29 20:45:37 -0400

    Update andras-script.py (#25)
    * Update andras-script.py
    
    * Update andras-script.py
    
    * Rename andras-script.py to andras_script.py

--- andras-script.py
+++
@@ -1,6 +0,0 @@
-# This is a test
-print("It works")
-
-but there is a syntax error here
-
-print("This should work, too")
---
+++ andras_script.py
@@ -0,0 +1,10 @@
+"""
+This is the module documentation.
+"""
+
+# This is a test
+print("It works")
+
+# but there is a syntax error here
+
+print("This should work, too")
