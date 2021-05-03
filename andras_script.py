b013ce1c608c3c1edcb62241f649ec43c4b843b3
Andras Lasso <lasso@queensu.ca>
2021-05-02 20:27:49 -0400

    Andras test with vscode

--- andras_script.py
+++ andras_script.py
@@ -11,3 +11,13 @@ print("This should work, too")
 
 # new line here
 
+"""
+This is the module documentation.
+"""
+
+# This is a test
+print("It works")
+
+# but there is a syntax error here
+
+print("This should work, too")
